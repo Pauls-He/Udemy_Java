33e084cbbd1add4de3df05467051c186d60429c0
Pauls-He <ph199209@gmail.com>
2023-08-10 14:13:37 +0300

    Update
    First steps

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args){
+    System.out.println("Hello, Pauls");
+    System.out.print("Hello World");
+
+    }
+}
--- HelloWorld/src/SecondClass.java
+++ HelloWorld/src/SecondClass.java
@@ -1,7 +1,5 @@
-public class FirstClass {
-
+public class SecondClass {
     public static void main(String[] args) {
         System.out.print("Hi, Pauls!");
     }
-
 }
