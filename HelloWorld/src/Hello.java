700eae7b0aa339eaadf71ad2daf508cd76a4d550
Pauls-He <ph199209@gmail.com>
2023-08-10 17:04:30 +0300

    Update Hello.java

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,7 +1,11 @@
 public class Hello {
     public static void main(String[] args){
     System.out.println("Hello, Pauls");
-    System.out.print("Hello World");
+     boolean isAlien = false;
+     if(isAlien == false) {
+         System.out.println("It is not an alient!");
+         System.out.println("And I am scared of aliens!");
+     }
 
     }
 }
