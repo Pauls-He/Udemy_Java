df37fb3c677d56d74b7b2183ef68c70a1942beb0
Pauls-He <ph199209@gmail.com>
2023-08-11 10:04:33 +0300

    lerning

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -7,5 +7,49 @@ public class Hello {
          System.out.println("And I am scared of aliens!");
      }
 
+     int topScore = 80;
+     if(topScore < 100){
+         System.out.println("You got the high score! "+ topScore);
+     }
+     int secondTopScore = 95;
+     if((topScore > secondTopScore) && (topScore < 100)) {
+         System.out.println("Greter than second top score and less than 100 ");
+     }
+     if(topScore > 90 || (secondTopScore <= 90)){
+         System.out.println("Either or both of the condition are true");
+     }
+     int newValue = 50;
+     if(newValue == 50){
+         System.out.println("This is true");
+
+     }
+     boolean isCar = false;
+     if(!isCar){
+         System.out.println("This is not supposed to happen");
+     }
+    String makeOfCar = "Volkswagen";
+     boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+     if(isDomestic){
+         System.out.println("This car is domestic to our country");
+     }
+    String s = (isDomestic) ? "This car is domestic" : "This car is not Domestic";
+
+        System.out.println(s);
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theReminder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoReminder = " + isNoRemainder);
+        if(!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+
+
+
     }
 }
