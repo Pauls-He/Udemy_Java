de46c12fd783860dd4ad409547809862dbc9ce98
Pauls-He <ph199209@gmail.com>
2023-08-31 16:11:07 +0300

    while loop

--- WhileLoopChallenge/src/Main.java
+++ WhileLoopChallenge/src/Main.java
@@ -1,2 +1,33 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 4;
+        int finishNumber = 20;
+        int evenCount = 0;
+        int oddCount = 0;
+
+        while (number <= finishNumber) {
+            number++;
+            if(!isEvenNumber(number)) {
+                oddCount++;
+                continue;
+            }
+            System.out.println("Even number " + number);
+            evenCount++;
+            if(evenCount >= 5) {
+                break;
+            }
+        }
+
+        System.out.println("Total odd number found = " + oddCount);
+        System.out.println("Total even number found = " + evenCount);
+    }
+    public static boolean isEvenNumber(int number) {
+        if((number % 2) == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
