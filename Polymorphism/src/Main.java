7a2664a507a67abaac731abb7616ee5e2ae8faf7
Pauls-He <ph199209@gmail.com>
2023-12-18 15:22:14 +0200

    Polymorphism

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -1,7 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+
 public class Main {
     public static void main(String[] args) {
 
-        Movie theMovie = new Adventure("Star Wars");
-        theMovie.watchMovie();
+//        Movie theMovie = Movie.getMovie("Science", "Star Wars");
+//        theMovie.watchMovie();
+
+        Scanner s = new Scanner(System.in);
+
+       List list = new ArrayList<String>();
+        while(true) {
+            System.out.print("Enter Type (A for Adventures, C for Comedy, " +
+                    "S for Science Fiction, or Q to quit): ");
+            String type = s.nextLine();
+            if("Qq".contains(type)){
+                break;
+            }
+            System.out.println("Enter Movie Title: ");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type,title);
+            movie.watchMovie();
+        }
     }
 }
--- Polymorphism/src/Movie.java
+++ Polymorphism/src/Movie.java
@@ -10,6 +10,15 @@ public class Movie {
         System.out.println(title + " is a " + instanceType + " film");
 
     }
+
+    public static Movie getMovie(String type, String title){
+        return switch (type.toUpperCase().charAt(0)){
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
 }
 class Adventure extends Movie{
     public Adventure(String title) {
@@ -23,6 +32,10 @@ class Adventure extends Movie{
                 "Pleasant Scene",
                 "Scary Music",
                 "Something Bad Happens");
+    }
+
+    public void watchAdventure(){
+        System.out.println("Watching an Adventure!");
     }
 }
 class Comedy extends Movie{
@@ -38,6 +51,9 @@ class Comedy extends Movie{
                 "Something even funnier happens",
                 "Happy Ending");
     }
+    public void watchComedy(){
+        System.out.println("Watching a Comedy!");
+    }
 }
 class ScienceFiction extends Movie{
     public ScienceFiction(String title) {
@@ -48,9 +64,12 @@ class ScienceFiction extends Movie{
     public void watchMovie() {
         super.watchMovie();
         System.out.printf(".. %s%n".repeat(3),
-                "Bad Aliens do Bad Sruff",
-                "Space Guys Chase Alliens",
+                "Bad Aliens do Bad Stuff",
+                "Space Guys Chase Aliens",
                 "Planet Blows Up");
+    }
+    public void watchScienceFiction(){
+        System.out.println("Watching a Science Fiction!");
     }
 }
 
