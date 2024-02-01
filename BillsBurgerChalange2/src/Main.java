afbfc671135f0c637c8e9bdec93d9d350193e686
Pauls-He <ph199209@gmail.com>
2024-02-01 18:49:30 +0200

    DeluxeBurger

--- BillsBurgerChalange2/src/Main.java
+++ BillsBurgerChalange2/src/Main.java
@@ -18,10 +18,16 @@ public class Main {
 ////        regularMeal.addBurgerToppings("BACON","CHEESE","MAYO");
 ////        regularMeal.setDrinkSize("LARGE");
 ////        regularMeal.printItemizedList();
-        MealOrder secondMeal = new MealOrder("turkey", "7-up", "chili");
-        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
-        secondMeal.setDrinkSize("SMALL");
-        secondMeal.printItemizedList();
+//        MealOrder secondMeal = new MealOrder("turkey", "7-up", "chili");
+//        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+//        secondMeal.setDrinkSize("SMALL");
+//        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up","chili");
+        deluxeMeal.addBurgerToppings("AVACADO", "BACON", "LETTUCE", "CHEESE", "MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
+
 
     }
 }
